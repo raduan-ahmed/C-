83c720ef73a7d84cbaabf8bfbb24f9b045e076f3
Md Raduan Ahamed <raduanahmed49591@gmail.com>
2024-12-23 01:42:28 +0600

    commit up

---
+++ 1-C++_Tutorial/4_5-real-life_example.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  // Create integer variables
+  int length = 4;
+  int width = 6;
+  int area;
+
+  // Calculate the area of a rectangle
+  area = length * width;
+
+  // Print the variables
+  cout << "Length is: " << length << "\n";
+  cout << "Width is: " << width << "\n";
+  cout << "Area of the rectangle is: " << area << "\n";
+
+  return 0;
+}
+
+
